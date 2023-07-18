9fcc5641c40fc8f06d9b529e0e33c8ebe7fab4b8
Chris Muntean <57206061+chrismuntean@users.noreply.github.com>
2023-07-18 11:54:10 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+model.predict(source="rtmp://sfs02-traveler.modot.mo.gov:1935/rtplive/MODOT_CAM_775", show=True)
+
+# Next step is to specify what type of vehicle (car, truck, or bus)
+# Then specify color
+# Finally specify body type/ make & model
