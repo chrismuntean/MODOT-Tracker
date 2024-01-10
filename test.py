fef9ef2c90fc57f0265d01f44c8a38e64729d5c4
Chris Muntean <57206061+chrismuntean@users.noreply.github.com>
2024-01-10 17:34:50 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-model.track(source="rtmp://sfs02-traveler.modot.mo.gov:1935/rtplive/MODOT_CAM_775", show=True)
-
-# Next step is to specify what type of vehicle (car, truck, or bus)
-# Then specify color
-# Finally specify body type/ make & model
+model(source="rtmp://sfs02-traveler.modot.mo.gov:1935/rtplive/MODOT_CAM_775", show=True, classes=2)
